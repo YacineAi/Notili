33375c252c3a6ca9f0cf5817808ac1e3ea9cd91e
Yacine Djenidi <djenidi.dev@gmail.com>
2024-01-06 21:53:30 +0100

    text then del

---
+++ exampls.js
@@ -0,0 +1,37 @@
+const { Telegraf, Markup } = require('telegraf');
+
+ctx.reply("Hi this test message with buttons", {
+    reply_markup:{
+      inline_keyboard:[
+        [
+          { text:"Frontend", callback_data:"Frontend"},
+          { text:"Backend", callback_data:"Backend"}
+        ]
+      ]
+    }
+});
+
+
+ctx.replyWithPhoto("https://ae01.alicdn.com/kf/S3e6e4d5a07894b9c840c15defc65c71cJ/Windbreaker-Men-Tactical-Jacket-Waterproof-Outdoor-Hooded-Coat-Sports-Military-European-Size-S-3xl-Field-Climbing.jpg", {
+    caption: 'This is an example of an image with text and buttons.',
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: 'Button 1', callback_data: 'button1' },
+          { text: 'Button 2', callback_data: 'button2' },
+        ],
+      ],
+    },
+});
+
+
+ctx.replyWithPhoto(
+    { url: "https://picsum.photos/200/300/?random" },
+    {
+        caption: "Caption",
+        parse_mode: "Markdown",
+        ...Markup.inlineKeyboard([
+            Markup.button.callback("Plain", "plain"),
+            Markup.button.url("Italic", "https://www.npmjs.com/"),
+        ])
+});
--- index.js
+++ index.js
@@ -11,38 +11,22 @@ bot.help((ctx) => {
 
 bot.on('text', (ctx) => {
   console.log(ctx.message.from);
-  ctx.reply("Hi this test message with buttons", {
-    reply_markup:{
-      inline_keyboard:[
-        [
-          { text:"Frontend", callback_data:"Frontend"},
-          { text:"Backend", callback_data:"Backend"}
-        ]
-      ]
-    }
+  ctx.reply('This message will be deleted in 5 seconds')
+  .then((message) => {
+    setTimeout(() => {
+      ctx.deleteMessage(message.message_id).then(() => {
+        ctx.replyWithPhoto({ url: "https://picsum.photos/200/300/?random" },
+        {
+          caption: "Caption",
+          parse_mode: "Markdown",
+          ...Markup.inlineKeyboard([
+            Markup.button.callback("Plain", "plain"),
+            Markup.button.url("Italic", "https://www.npmjs.com/"),
+          ])
+        });
+      })
+    }, 5000)
   });
-  ctx.replyWithPhoto("https://ae01.alicdn.com/kf/S3e6e4d5a07894b9c840c15defc65c71cJ/Windbreaker-Men-Tactical-Jacket-Waterproof-Outdoor-Hooded-Coat-Sports-Military-European-Size-S-3xl-Field-Climbing.jpg", {
-    caption: 'This is an example of an image with text and buttons.',
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: 'Button 1', callback_data: 'button1' },
-          { text: 'Button 2', callback_data: 'button2' },
-        ],
-      ],
-    },
-  });
-
-  ctx.replyWithPhoto(
-		{ url: "https://picsum.photos/200/300/?random" },
-		{
-			caption: "Caption",
-			parse_mode: "Markdown",
-			...Markup.inlineKeyboard([
-				Markup.button.callback("Plain", "plain"),
-				Markup.button.url("Italic", "https://www.npmjs.com/"),
-			])
-		});
 });
 
 bot.launch({ webhook: { domain: process.env.RENDER_EXTERNAL_URL, port: process.env.PORT } })
