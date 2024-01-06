652cea4350787b7918460ffb9e0b589a01e8b395
Yacine Djenidi <djenidi.dev@gmail.com>
2024-01-06 21:28:45 +0100

    text

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Telegraf } = require('telegraf');
+const { Markup } = require('telegraf');
 const bot = new Telegraf(process.env.TELETOKEN);
 
 bot.start((ctx) => {
@@ -11,7 +12,30 @@ bot.help((ctx) => {
 
 bot.on('text', (ctx) => {
   console.log(ctx.message.from);
-  ctx.reply(`You said: "${ctx.message.text}"`);
+  ctx.reply("Hi this test message with buttons", {
+    reply_markup:{
+      inline_keyboard:[
+        [
+          { text:"Frontend", callback_data:"Frontend"},
+          { text:"Backend", callback_data:"Backend"}
+        ]
+      ]
+    }
+  });
+
+  ctx.replyWithPhoto(
+    { source: 'https://ae01.alicdn.com/kf/S3e6e4d5a07894b9c840c15defc65c71cJ/Windbreaker-Men-Tactical-Jacket-Waterproof-Outdoor-Hooded-Coat-Sports-Military-European-Size-S-3xl-Field-Climbing.jpg' },
+    {
+      caption: 'This is an example of an image with text and buttons.',
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: 'Button 1', callback_data: 'button1' },
+            { text: 'Button 2', callback_data: 'button2' },
+          ],
+        ],
+      },
+    });
 });
 
 bot.launch({ webhook: { domain: process.env.RENDER_EXTERNAL_URL, port: process.env.PORT } })
